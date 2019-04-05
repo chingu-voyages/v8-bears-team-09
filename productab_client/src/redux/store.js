5511ea50790bdfa199764ea4e6ca96920f0138c6
linh4 <linh44h@gmail.com>
2019-04-04 23:14:47 -0400

    update README

--- productab_client/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-import { createStore, applyMiddleware}  from "redux";
-import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
-import rootReducer from "./reducers/root";
-
-const middleware = applyMiddleware(thunk);
-const store = createStore(rootReducer, composeWithDevTools(middleware));
-export default store;
